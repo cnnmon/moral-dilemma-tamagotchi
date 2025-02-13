571dee93a400bd15b0a91025998bb777c2c791d9
cnnmon <tiffanywang@berkeley.edu>
2025-02-12 17:34:23 -0800

    animations

--- app/play/page.tsx
+++ app/play/page.tsx
@@ -3,13 +3,13 @@
 import { useQuery } from "convex/react";
 import { api } from "@/convex/_generated/api";
 import { OutcomePopup } from "./components/Outcome";
-import { Footer } from "./components/Footer";
 import Loading from "./components/Loading";
 import { useOutcomes } from "./utils/useOutcomes";
 import { useCurrentDilemma } from "./utils/useCurrentDilemma";
 import Viewport from "./components/Viewport";
 import DilemmaDisplay from "./components/DilemmaDisplay";
 import Stats from "./components/Stats";
+import { AnimatePresence, motion } from "framer-motion";
 
 export default function Play() {
   const stateResult = useQuery(api.state.getActiveGameState);
@@ -52,41 +52,53 @@ export default function Play() {
   const { pet, seenDilemmas } = stateResult;
 
   return (
-    <div className="flex flex-col items-center gap-8 justify-center p-4 md:p-0 md:w-xl w-full">
-      {/* Displays setting buttons */}
-      <Footer />
+    <AnimatePresence mode="wait">
+      <div className="flex flex-col items-center gap-8 justify-center p-4 md:p-0 md:w-xl w-full">
+        {/* Displays stats */}
+        <motion.div
+          className="md:absolute w-full h-full"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 0.5, delay: 0.1 }}
+        >
+          <Stats pet={pet} seenDilemmasCount={seenDilemmas.length} />
+        </motion.div>
 
-      {/* Displays stats */}
-      <Stats pet={pet} seenDilemmasCount={seenDilemmas.length} />
+        {/* Displays outcomes */}
+        <div className="fixed top-0 p-4 w-full max-w-xl z-10">
+          {outcomes.map((outcome) => (
+            <OutcomePopup
+              key={outcome.id}
+              message={outcome.text}
+              exitable={outcome.exitable}
+              onClose={() => removeOutcome(outcome.id)}
+            />
+          ))}
+        </div>
 
-      {/* Displays outcomes */}
-      <div className="fixed top-0 p-4 w-full max-w-xl z-10">
-        {outcomes.map((outcome) => (
-          <OutcomePopup
-            key={outcome.id}
-            message={outcome.text}
-            exitable={outcome.exitable}
-            onClose={() => removeOutcome(outcome.id)}
+        {/* Displays the pet & background */}
+        <Viewport
+          clarifyingQuestion={
+            status === "has_unresolved_dilemma" ? stateResult.question : null
+          }
+        />
+
+        {/* Displays dilemma */}
+        <motion.div
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.5, delay: 0.5 }}
+        >
+          <DilemmaDisplay
+            pet={pet}
+            dilemma={currentDilemma}
+            onOutcome={addOutcome}
+            onProcessingStart={onDilemmaProcessingStart}
+            onProcessingEnd={onDilemmaProcessingEnd}
+            disabled={isProcessing}
           />
-        ))}
+        </motion.div>
       </div>
-
-      {/* Displays the pet & background */}
-      <Viewport
-        clarifyingQuestion={
-          status === "has_unresolved_dilemma" ? stateResult.question : null
-        }
-      />
-
-      {/* Displays dilemma */}
-      <DilemmaDisplay
-        pet={pet}
-        dilemma={currentDilemma}
-        onOutcome={addOutcome}
-        onProcessingStart={onDilemmaProcessingStart}
-        onProcessingEnd={onDilemmaProcessingEnd}
-        disabled={isProcessing}
-      />
-    </div>
+    </AnimatePresence>
   );
 }
