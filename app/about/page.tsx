bb2aef5148058cc6594061f67be56694456506c2
cnnmon <tiffanywang@berkeley.edu>
2025-03-11 00:10:35 -0700

    path

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,30 +1,25 @@
 "use client";
 
-import Window from "@/components/Window";
-
 export default function About() {
   return (
     <div className="flex flex-col items-center justify-center p-4 sm:p-0 sm:w-xl w-full">
-      <Window title="about">
-        <div className="flex flex-col gap-2">
-          <p>thanks for playing~</p>
-          <p>[work in progress, give me feedback!]</p>
-          <p>
-            princi(pal) is a moral dilemma tamagotchi. it&apos;s a virtual pet
-            game where you can (finally!) impose your own ethical views on said
-            pet and watch the consequences unfold
-          </p>
-          <p>
-            made with{" "}
-            <a href="https://github.com/cnnmon/moral-dilemma-tamagotchi">
-              moral uncertainty
-            </a>{" "}
-            by <a href="https://tiffanywang.me/">chadd</a>
-            <br />
-            big thanks to gavin for dilemma writing help (*ˊᵕˋ)
-          </p>
-        </div>
-      </Window>
+      <div className="flex flex-col gap-2">
+        <p>thanks for playing~</p>
+        <p>
+          princi(pal) is a moral dilemma tamagotchi. it&apos;s a virtual pet
+          game where you can (finally!) impose your own ethical views on said
+          pet and watch the consequences unfold
+        </p>
+        <p>
+          made with{" "}
+          <a href="https://github.com/cnnmon/moral-dilemma-tamagotchi">
+            moral uncertainty
+          </a>{" "}
+          by <a href="https://tiffanywang.me/">tiff</a>
+          <br />
+          big thanks to gavin for dilemma writing help (*ˊᵕˋ)
+        </p>
+      </div>
     </div>
   );
 }
--- components/Menu.tsx
+++ components/Menu.tsx
@@ -6,7 +6,6 @@ import { api } from "@/convex/_generated/api";
 import { useQuery } from "convex/react";
 
 export default function Menu() {
-  const [menuOpen, setMenuOpen] = useState(false);
   const [currentPath, setCurrentPath] = useState("");
   const currentPet = useQuery(api.pets.getActivePet);
 
@@ -16,56 +15,42 @@ export default function Menu() {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [window.location.pathname]);
 
+  const pathToText = {
+    play: "home > living room",
+    create: "adoption center",
+    about: "about princi/pal",
+  };
+
   return (
-    <div className="fixed top-0 left-0 text-sm text-zinc-500 p-4 z-10">
-      <div className="relative">
-        <a
-          onClick={() => setMenuOpen(!menuOpen)}
-          className="hover:text-zinc-800 no-drag"
+    <div className="fixed top-0 text-sm text-zinc-500 p-4 z-10 flex gap-4 justify-between w-full">
+      <AnimatePresence>
+        <motion.span
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 0.3, delay: 0.1 }}
+          className="ml-2"
         >
-          home
-        </a>
-        <AnimatePresence mode="wait">
-          {menuOpen && (
-            <motion.div
-              initial={{ opacity: 0, rotateX: -90 }}
-              animate={{ opacity: 1, rotateX: 0 }}
-              exit={{ opacity: 0, rotateX: 90 }}
-              transition={{ duration: 0.2 }}
-              className="absolute top-full left-0 mt-1 bg-white p-2 gap-1 flex flex-col w-30"
-              style={{ transformOrigin: "top" }}
-            >
-              {currentPath !== "create" && (
-                <a href="/create" className="block py-1 hover:bg-zinc-100">
-                  adopt a new pet
-                </a>
-              )}
-              {currentPet && currentPath !== "play" && (
-                <a href="/play" className="block py-1 hover:bg-zinc-100">
-                  resume: {currentPet.name}
-                </a>
-              )}
-              {currentPath !== "about" && (
-                <a href="/about" className="block py-1 hover:bg-zinc-100">
-                  about
-                </a>
-              )}
-            </motion.div>
-          )}
-        </AnimatePresence>
-        <AnimatePresence>
-          <motion.span
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            transition={{ duration: 0.3, delay: 0.1 }}
-          >
-            {` > ${currentPath}${
-              currentPet && currentPath === "play"
-                ? ` > ${currentPet.name}`
-                : ""
-            }`}
-          </motion.span>
-        </AnimatePresence>
+          {`${pathToText[currentPath as keyof typeof pathToText]}${
+            currentPet && currentPath === "play" ? ` > ${currentPet.name}` : ""
+          }`}
+        </motion.span>
+      </AnimatePresence>
+      <div className="flex gap-2">
+        {currentPet && currentPath !== "play" && (
+          <a href="/play" className="hover:text-zinc-800 no-drag">
+            resume game: {currentPet.name}
+          </a>
+        )}
+        {currentPath !== "create" && (
+          <a href="/create" className="hover:text-zinc-800 no-drag">
+            new pet
+          </a>
+        )}
+        {currentPath !== "about" && (
+          <a href="/about" className="hover:text-zinc-800 no-drag">
+            about
+          </a>
+        )}
       </div>
     </div>
   );
