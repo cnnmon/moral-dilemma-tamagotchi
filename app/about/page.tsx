de1e90cf6041279f6cb6c59959cb7254c01b5a82
cnnmon <tiffanywang@berkeley.edu>
2025-03-11 01:33:54 -0700

    Update page.tsx

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,9 +1,16 @@
-"use client";
+import Image from "next/image";
 
 export default function About() {
   return (
     <div className="flex flex-col items-center justify-center p-4 sm:p-0 sm:w-xl w-full">
       <div className="flex flex-col gap-2">
+        <Image
+          src="/poo.gif"
+          alt="princi(pal)"
+          width={50}
+          height={50}
+          className="rounded-full"
+        />
         <p>thanks for playing~</p>
         <p>
           princi(pal) is a moral dilemma tamagotchi. it&apos;s a virtual pet
